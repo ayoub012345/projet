d4e598d374d1e022e5ee3bc987dc95328272db67
ayoub012345 <122547371+ayoub012345@users.noreply.github.com>
2023-02-16 16:39:09 +0100

    Add files via upload

---
+++ prog.js
@@ -0,0 +1,10 @@
+function student_list()
+{
+    var x = document.getElementById('frot_');
+    x.setAttribute('src','student_list.html')
+}
+function courses()
+{
+    var x = document.getElementById('frot_');
+    x.setAttribute('src','modul.html')
+}
